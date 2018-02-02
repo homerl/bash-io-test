451df7bea12cdaa6a3cc21256a46dd926a6b7354
Homer Li <01jay.ly@gmail.com>
2018-02-02 21:02:54 +0800

    Update meta-test.sh

--- meta-test.sh
+++ meta-test.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
+###########################################################################
+# Script:       $0
+# Author:       Homer Li
+# Modify:       Homer Li
+# Date:         2018-1-26
+# Update:       2018-1-26
+# Email:        liyan2@genomics.org.cn
+# Usage:        $0
+# Discription:  test posix meta data performance
+#
+###########################################################################
 ipaddr=$(ip a | awk --posix -F '[ /]+' 'BEGIN{i=0};$0~/[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}/ && $0~/inet/ && $0!~/inet6/ && $0!~/10.53.27/ && $0!~/10.53.28/ && $0!~/0.0.0.0/ && $0!~/127.0.0.1/ && $0!~/00:00/ {if(i==0) print $3;i++}')
 resdir=/dev/shm/$ipaddr/mdtest
 mkdir -p $resdir
@@ -31,31 +42,34 @@ metatest() {
   mkdir -p $workdir; cd $workdir
   time (touch $3{0..65535} && touch $3{65536..131070} && touch $3{131071..196605} && touch $3{196606..262140} ) > $resdir/touch_${1} 2>&1
   echo 3 > /proc/sys/vm/drop_caches
-  time (ls -l > /dev/zero) > $resdir/ls_${1} 2>&1
+  time (mkdir $3{0..65535} && mkdir $3{65536..131070} && mkdir $3{131071..196605} && mkdir $3{196606..262140} ) > $resdir/mkdir_${1} 2>&1
   echo 3 > /proc/sys/vm/drop_caches
-  time (ls | xargs stat) > $resdir/stat_${1} 2>&1
+  #time (ls -l > /dev/zero) > $resdir/ls_${1} 2>&1
+  time (ls -l > /dev/zero) > /dev/zero 2>&1
   echo 3 > /proc/sys/vm/drop_caches
-  time (setfacl -m u:nobody:rwx,g:nobody:rwx ./*) > $resdir/setacl_${1} 2>&1
+  time (ls | xargs stat) > /dev/zero 2>&1
   echo 3 > /proc/sys/vm/drop_caches
-  time (getfacl ./*) > $resdir/getacl_${1} 2>&1
+  time (setfacl -m u:nobody:rwx,g:nobody:rwx ./*) > /dev/zero 2>&1
   echo 3 > /proc/sys/vm/drop_caches
-  time (chown root.nobody ./*) > $resdir/chown_${1} 2>&1
+  time (getfacl ./*) > /dev/zero 2>&1
   echo 3 > /proc/sys/vm/drop_caches
-  time (chmod g+s ./*) > $resdir/chmod_${1} 2>&1
+  time (chown root.nobody ./*) > /dev/zero 2>&1
   echo 3 > /proc/sys/vm/drop_caches
-  time (setfattr -n user.comment -v "this is a comment" ./*) > $resdir/setfattr_${1} 2>&1
+  time (chmod g+s ./*) > /dev/zero 2>&1
   echo 3 > /proc/sys/vm/drop_caches
-  time (getfattr ./* -n user.comment) > $resdir/getfattr_${1} 2>&1
+  time (setfattr -n user.comment -v "this is a comment" ./*) > /dev/zero 2>&1
   echo 3 > /proc/sys/vm/drop_caches
-  time (chattr +ai ./*) > $resdir/chattr_add_${1} 2>&1
+  time (getfattr ./* -n user.comment) > /dev/zero 2>&1
   echo 3 > /proc/sys/vm/drop_caches
-  time (chattr -ai ./*) > $resdir/chattr_rm_${1} 2>&1
+  time (chattr +ai ./*) > /dev/zero 2>&1
   echo 3 > /proc/sys/vm/drop_caches
-  time (lsattr -v ./*) > $resdir/lsattr_${1} 2>&1
+  time (chattr -ai ./*) > /dev/zero 2>&1
   echo 3 > /proc/sys/vm/drop_caches
-  time (mkdir test10;mv ./* test10) > $resdir/mv_${1} 2>&1
+  time (lsattr -v ./*) > /dev/zero 2>&1
+  echo 3 > /proc/sys/vm/drop_caches
+  time (mkdir test10;mv ./* test10) > /dev/zero 2>&1
   #echo 3 > /proc/sys/vm/drop_caches
-  #time (cd /mnt/$ipaddr/test100 && rm -rf ./test10) > $resdir/rm_${1} 2>&1
+  #time (cd /mnt/$ipaddr/test100 && rm -rf ./test10) > /dev/zero 2>&1
 }
 [[ -z $Npro ]] && export Npro=20
 [[ -z $total ]] && export total=100

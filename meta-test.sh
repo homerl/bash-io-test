a25a33bc0317257ce2065387f525898b2b99312d
Homer Li <01jay.ly@gmail.com>
2018-04-03 20:00:55 +0800

    Update meta-test.sh
    add dir , acl, xattr test

--- meta-test.sh
+++ meta-test.sh
@@ -4,7 +4,7 @@
 # Author:       Homer Li
 # Modify:       Homer Li
 # Date:         2018-1-26
-# Update:       2018-1-26
+# Update:       2018-4-03
 # Email:        liyan2@genomics.org.cn
 # Usage:        $0
 # Discription:  test posix meta data performance
@@ -12,8 +12,7 @@
 ###########################################################################
 ipaddr=$(ip a | awk --posix -F '[ /]+' 'BEGIN{i=0};$0~/[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}/ && $0~/inet/ && $0!~/inet6/ && $0!~/10.53.27/ && $0!~/10.53.28/ && $0!~/0.0.0.0/ && $0!~/127.0.0.1/ && $0!~/00:00/ {if(i==0) print $3;i++}')
 resdir=/dev/shm/$ipaddr/mdtest
-mkdir -p $resdir
-
+[[ ! -d $resdir ]] && mkdir -p $resdir
 usage() {
         echo "Usage: $0 [-d test dir] [-p process number] [-j runing jobs]" 2>&1; exit 1;
 }
@@ -35,41 +34,54 @@ while getopts ":d:p:j:" o; do
     esac
 done
 
+ulimit -s 1310720
 [[ -z $testdir ]] && usage && exit 1
 
 metatest() {
   workdir=$2"/test"$1
-  mkdir -p $workdir; cd $workdir
-  time (touch $3{0..65535} && touch $3{65536..131070} && touch $3{131071..196605} && touch $3{196606..262140} ) > $resdir/touch_${1} 2>&1
-  echo 3 > /proc/sys/vm/drop_caches
-  time (mkdir $3{0..65535} && mkdir $3{65536..131070} && mkdir $3{131071..196605} && mkdir $3{196606..262140} ) > $resdir/mkdir_${1} 2>&1
-  echo 3 > /proc/sys/vm/drop_caches
-  #time (ls -l > /dev/zero) > $resdir/ls_${1} 2>&1
-  time (ls -l > /dev/zero) > /dev/zero 2>&1
-  echo 3 > /proc/sys/vm/drop_caches
-  time (ls | xargs stat) > /dev/zero 2>&1
-  echo 3 > /proc/sys/vm/drop_caches
-  time (setfacl -m u:nobody:rwx,g:nobody:rwx ./*) > /dev/zero 2>&1
-  echo 3 > /proc/sys/vm/drop_caches
-  time (getfacl ./*) > /dev/zero 2>&1
-  echo 3 > /proc/sys/vm/drop_caches
-  time (chown root.nobody ./*) > /dev/zero 2>&1
-  echo 3 > /proc/sys/vm/drop_caches
-  time (chmod g+s ./*) > /dev/zero 2>&1
-  echo 3 > /proc/sys/vm/drop_caches
-  time (setfattr -n user.comment -v "this is a comment" ./*) > /dev/zero 2>&1
-  echo 3 > /proc/sys/vm/drop_caches
-  time (getfattr ./* -n user.comment) > /dev/zero 2>&1
-  echo 3 > /proc/sys/vm/drop_caches
-  time (chattr +ai ./*) > /dev/zero 2>&1
-  echo 3 > /proc/sys/vm/drop_caches
-  time (chattr -ai ./*) > /dev/zero 2>&1
-  echo 3 > /proc/sys/vm/drop_caches
-  time (lsattr -v ./*) > /dev/zero 2>&1
-  echo 3 > /proc/sys/vm/drop_caches
-  time (mkdir test10;mv ./* test10) > /dev/zero 2>&1
-  #echo 3 > /proc/sys/vm/drop_caches
-  #time (cd /mnt/$ipaddr/test100 && rm -rf ./test10) > /dev/zero 2>&1
+  [[ ! -d $workdir ]] && mkdir -p $workdir; cd $workdir
+  set -e
+  cd $workdir
+  (touch $3{0..65535} && touch $3{65536..131070} && touch $3{131071..196605} && touch $3{196606..262140} )
+  ### for test #(touch $3{0..15})
+  randv=$(openssl rand -hex 4)
+  ([[ ! -d dir ]] && mkdir dir; cd dir && for i in {0..300}; do mkdir -p $(echo ${randv}_{0..500} | tr " " "/"); done) > /dev/zero
+  ### for test#([[ ! -d dir ]] && mkdir dir; cd dir && for i in {0..3}; do mkdir -p $(echo ${randv}_{0..5} | tr " " "/"); done) > /dev/zero
+  cd $workdir
+  time (ls -l > /dev/zero) > $resdir/ls_${1}
+  (ls -lR)
+  (ls | xargs stat)
+  (setfacl -b $workdir/*)
+  (setfacl -m u:nobody:rwx,g:nobody:rwx $workdir/*)
+  (setfacl -m u:mail:rwx,g:nobody:rwx $workdir/*)
+  (setfacl -m u:root:rwx,g:root:rwx $workdir/*)
+  (setfacl -m u:bin:rwx,g:bin:rwx $workdir/*)
+  (setfacl -m u:sshd:rwx,g:sshd:rwx $workdir/*)
+  (setfacl -m u:ntp:rwx,g:ntp:rwx $workdir/*)
+  (setfacl -m u:rpc:rwx,g:rpc:rwx $workdir/*)
+  (setfacl -m u:nfsnobody:rwx,g:nfsnobody:rwx $workdir/*)
+  (setfacl -m u:rpcuser:rwx,g:rpcuser:rwx $workdir/*)
+  (setfacl -m u:adm:rwx,g:adm:rwx $workdir/*)
+  (setfacl -m u:daemon:rwx,g:daemon:rwx $workdir/*)
+  (setfacl -m u:ftp:rwx,g:ftp:rwx $workdir/*)
+  (setfacl -m u:operator:rwx,g:root:rwx $workdir/*)
+  (getfacl $workdir/*)
+  (chown root.nobody $workdir/*)
+  (chmod g+s $workdir/*)
+  (setfattr -n user.comment -v "this is a long comment $(openssl rand -hex 64)" $workdir/*)
+  (setfattr -n user.checksum -v "md5 checksum $(openssl rand -hex 64)" $workdir/*)
+  (setfattr -n user.result_1000 -v "the others $(openssl rand -hex 64)" $workdir/*)
+  (getfattr $workdir/* -n user.comment)
+  (getfattr $workdir/* -n user.checksum)
+  (getfattr $workdir/* -n user.result_1000)
+  (setfattr -x user.result_1000 $workdir/*)
+  (chattr +ai $workdir/*)
+  (chattr -ai $workdir/*)
+  (lsattr -v $workdir/*)
+  value=$RANDOM
+  (mkdir test${value};mv ./* test${value}) > /dev/zero 2>&1
+  ([[ -n $testdir ]] && cd $testdir && rm -rf ./test${value}) > /dev/zero 2>&1
+  set +e
 }
 [[ -z $Npro ]] && export Npro=20
 [[ -z $total ]] && export total=100
@@ -86,8 +98,7 @@ do
         read -u7
         {
 
-                [[ $(getconf ARG_MAX) -gt 2097152 ]] && filehead="11111112222222333333333444444445555555555677777777777zzzzzzzzzkriiiiiiiiiiiiiiiiiiiiiiiiiiiii"
-                [[ $(getconf ARG_MAX) -le 2097152 ]] && filehead="hahahahahahahahahaha"
+                filehead=$(openssl rand -hex 32)
                 metatest $i $testdir $filehead
                 echo >&7
         } &

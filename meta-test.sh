895e79c014977e338338d85ad0f882c476a19fed
Homer Li <01jay.ly@gmail.com>
2018-04-20 15:04:53 +0800

    Update meta-test.sh
    Add read / write little data to all files,  improve read write performance

--- meta-test.sh
+++ meta-test.sh
@@ -4,7 +4,7 @@
 # Author:       Homer Li
 # Modify:       Homer Li
 # Date:         2018-1-26
-# Update:       2018-4-04
+# Update:       2018-4-20
 # Email:        liyan2@genomics.org.cn
 # Usage:        $0
 # Discription:  test posix meta data performance
@@ -43,50 +43,51 @@ ulimit -s 1310720
 metatest() {
   workdir=$2"/test"$1
   [[ ! -d $workdir ]] && mkdir -p $workdir; cd $workdir
-  set -e
   cd $workdir
   #(touch $3{0..65535} && touch $3{65536..131070} && touch $3{131071..196605} && touch $3{196606..262140} )
-  j=0;for ((i=0;i<=8388480;i=i+1048560));  do j=$((j==0?0:((j+1))));[[ $i -gt 0 ]] && echo "touch {"$j".."$i"}";j=$i; done | bash
-  ### for test #(touch $3{0..15})
-  randv=$(openssl rand -hex 4)
-  ([[ ! -d dir ]] && mkdir dir; cd dir && for i in {0..300}; do mkdir -p $(echo ${randv}_{0..500} | tr " " "/"); done) > /dev/zero
-  ### for test#([[ ! -d dir ]] && mkdir dir; cd dir && for i in {0..3}; do mkdir -p $(echo ${randv}_{0..5} | tr " " "/"); done) > /dev/zero
+  j=0;for ((i=0;i<=2000000;i=i+1000000));  do j=$((j==0?0:((j+1))));[[ $i -gt 0 ]] && echo "touch $(openssl rand -hex 4)_{"$j".."$i"}";j=$i; done | bash  > /dev/zero
+  echo "---touch return---"$?
+  ### for test #(touch $3{0..15}) > /dev/zero
+  ([[ ! -d dir ]] && mkdir dir; cd dir && for i in {0..300}; do mkdir -p $(echo $(openssl rand -hex 4)_{0..200} | tr " " "/"); done) > /dev/zero > /dev/zero
+  echo "---mkdir return---"$?
+  ### for test#([[ ! -d dir ]] && mkdir dir; cd dir && for i in {0..3}; do mkdir -p $(echo ${randv}_{0..5} | tr " " "/"); done) > /dev/zero > /dev/zero
   cd $workdir
-  time (ls -l > /dev/zero) > $resdir/ls_${1}
-  (ls -lR)
-  (ls | xargs stat)
-  (setfacl -b $workdir/*)
-  (setfacl -m u:nobody:rwx,g:nobody:rwx $workdir/*)
-  (setfacl -m u:mail:rwx,g:nobody:rwx $workdir/*)
-  (setfacl -m u:root:rwx,g:root:rwx $workdir/*)
-  (setfacl -m u:bin:rwx,g:bin:rwx $workdir/*)
-  (setfacl -m u:sshd:rwx,g:sshd:rwx $workdir/*)
-  (setfacl -m u:ntp:rwx,g:ntp:rwx $workdir/*)
-  (setfacl -m u:rpc:rwx,g:rpc:rwx $workdir/*)
-  (setfacl -m u:nfsnobody:rwx,g:nfsnobody:rwx $workdir/*)
-  (setfacl -m u:rpcuser:rwx,g:rpcuser:rwx $workdir/*)
-  (setfacl -m u:adm:rwx,g:adm:rwx $workdir/*)
-  (setfacl -m u:daemon:rwx,g:daemon:rwx $workdir/*)
-  (setfacl -m u:ftp:rwx,g:ftp:rwx $workdir/*)
-  (setfacl -m u:operator:rwx,g:root:rwx $workdir/*)
-  (getfacl $workdir/*)
-  (chown root.nobody $workdir/*)
-  (chmod g+s $workdir/*)
-  (setfattr -n user.comment -v "this is a long comment $(openssl rand -hex 64)" $workdir/*)
-  (setfattr -n user.checksum -v "md5 checksum $(openssl rand -hex 64)" $workdir/*)
-  (setfattr -n user.result_1000 -v "the others $(openssl rand -hex 64)" $workdir/*)
-  (getfattr $workdir/* -n user.comment)
-  (getfattr $workdir/* -n user.checksum)
-  (getfattr $workdir/* -n user.result_1000)
-  (setfattr -x user.result_1000 $workdir/*)
-  (chattr +ai $workdir/*)
-  (chattr -ai $workdir/*)
-  (lsattr -v $workdir/*)
-  value=$RANDOM
-  (mkdir test${value};mv ./* test${value})
-  (du -hs test${value})
-  ([[ $nrm -eq 0 ]] && [[ -n $testdir ]] && cd $testdir && rm -rf ./test${value})
-  set +e
+  time (ls | xargs stat) > /dev/zero
+  echo "---ls stat return---"$?
+  time (setfacl -b $workdir/*) > /dev/zero
+  echo "---setfacl 1 return---"$?
+  time (setfacl -m u:nobody:rwx,g:nobody:rwx $workdir/*) > /dev/zero
+  echo "---setfacl 2 return---"$?
+  time (getfacl $workdir/*) > /dev/zero
+  echo "---getfacl return---"$?
+  time (chown root.nobody $workdir/*) > /dev/zero
+  echo "---chown return---"$?
+  time (chmod g+s $workdir/*) > /dev/zero
+  echo "---chmod return---"$?
+  time (setfattr -n user.comment -v "this is a long comment $(openssl rand -hex 64)" $workdir/*) > /dev/zero
+  echo "---setfattr 1 return---"$?
+  time (setfattr -n user.checksum -v "md5 checksum $(openssl rand -hex 64)" $workdir/*) > /dev/zero
+  echo "---setfattr 2 return---"$?
+  time (getfattr $workdir/* -n user.comment) > /dev/zero
+  echo "---getfattr 1 return---"$?
+  time (setfattr -x user.checksum $workdir/*) > /dev/zero
+  echo "---setfattr 3 return---"$?
+  time (chattr +ai $workdir/*) > /dev/zero
+  echo "---chattr 1 return---"$?
+  time (chattr -ai $workdir/*) > /dev/zero
+  echo "---chattr 2 return---"$?
+  time (echo $(openssl rand -hex 133) | tee -a $workdir/*) > /dev/zero
+  echo "---append return---"$?
+  time (cat $workdir/* ) > /dev/zero
+  echo "---read return---"$?
+  time (cat $workdir/* ) > /dev/zero
+  value=$RANDOM > /dev/zero
+  time (mkdir test${value};mv ./* test${value}) > /dev/zero
+  echo "---mv return---"$?
+  time (du -hs test${value}) > /dev/shm/meta_test_du_complete > /dev/zero
+  echo "---du return---"$?
+  time ([[ $nrm -eq 0 ]] && [[ -n $testdir ]] && cd $testdir && rm -rf ./test${value}) > /dev/zero
+  echo "---rm return---"$?
 }
 [[ -z $Npro ]] && export Npro=8
 [[ -z $total ]] && export total=16
